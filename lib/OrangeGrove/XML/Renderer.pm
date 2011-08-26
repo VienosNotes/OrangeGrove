eb7bfdb9faf36db1ea6003d6f5edad5a436170e7
vieno <scarletwine_vieno@yahoo.co.jp>
2011-08-26 18:19:10 +0900

    メッセージボックスを表示しない処理を追加

--- lib/OrangeGrove/XML/Renderer.pm
+++ lib/OrangeGrove/XML/Renderer.pm
@@ -74,60 +74,62 @@ sub _build {
                     ) or die $bg->errstr;
     }
 
-    #メッセージボックスのロード
-    my $msgbox = Imager->new;
-    $msgbox->read(file => $self->proj . "fig/" . $page->config->msgbox->{fig} . ".png") or die $msgbox->errstr;
+    #名前も文章もnoneならメッセージボックスを表示しない
+    unless ($page->name eq "none" && $page->msg eq "none") {
+        #メッセージボックスのロード
+        my $msgbox = Imager->new;
+        $msgbox->read(file => $self->proj . "fig/" . $page->config->msgbox->{fig} . ".png") or die $msgbox->errstr;
 
 
-    #フォントのロード
-    my $font = Imager::Font->new(file => $self->proj . $page->config->character->{font} . ".ttf",
-                              size => $page->config->character->{size},
-                              aa => 1,
-                              color => Imager::Color->new(r => 255, g => 255, b => 255)
-                          );
+        #フォントのロード
+        my $font = Imager::Font->new(file => $self->proj . $page->config->character->{font} . ".ttf",
+                                     size => $page->config->character->{size},
+                                     aa => 1,
+                                     color => Imager::Color->new(r => 255, g => 255, b => 255)
+                                 );
 
-    die "フォント見つからないよ" unless defined $font;
+        die "フォント見つからないよ" unless defined $font;
 
-    #テキストの生成
-    my $text = $page->msg;
-    $text =~ s/\A\n//;
-    $text =~ s/\t+//g;
+        #テキストの生成
+        my $text = $page->msg;
+        $text =~ s/\A\n//;
+        $text =~ s/\t+//g;
 
-    unless ($page->name eq "none") {
-        #名前からタブと二文字以上の空白の除去
-        my $name = $page->name;
-        $name =~ s/\n//g;
-        $name =~ s/\t+//g;
-        $name =~ s/\s\s+//g;
-        $text = " - " . $name . " - \n" . $text;
-    } else {
-        $text = "\n" . $text;
+        unless ($page->name eq "none") {
+            #名前からタブと二文字以上の空白の除去
+            my $name = $page->name;
+            $name =~ s/\n//g;
+            $name =~ s/\t+//g;
+            $name =~ s/\s\s+//g;
+            $text = " - " . $name . " - \n" . $text;
+        } else {
+            $text = "\n" . $text;
+        }
+
+        my $tb = Imager::DTP::Textbox::Horizontal->new(text => $text,
+                                                       font => $font,
+                                                       wspace => 1,
+                                                       leading => 130,
+                                                       halign => "left",
+                                                       valign => "top",
+                                                       wrapWidth => int $msgbox->getwidth - ($msgbox->getwidth * ($page->config->msgbox->{xmergin} / 50)),
+                                                       wrapHeight => int $msgbox->getheight - ($msgbox->getheight * ($page->config->msgbox->{ymergin} / 50)),
+                                                       xscale => 1,
+                                                       yscale => 1
+                                                   );
+
+        #テキストの合成
+        $tb->draw(target => $msgbox,
+                  x => $msgbox->getwidth * ($page->config->msgbox->{xmergin} / 100),
+                  y => $msgbox->getheight * ($page->config->msgbox->{ymergin} /100),
+              );
+
+        #メッセージボックスの合成
+        $bg->rubthrough(src => $msgbox,
+                        tx => $page->config->size->{x} * ($page->config->msgbox->{x} / 100) - $msgbox->getwidth/2,
+                        ty => $page->config->size->{"y"} * ($page->config->msgbox->{"y"} / 100) - $msgbox->getheight/2,
+                    ) or die $bg->errstr;
     }
-
-    my $tb = Imager::DTP::Textbox::Horizontal->new(text => $text,
-                                                   font => $font,
-                                                   wspace => 1,
-                                                   leading => 130,
-                                                   halign => "left",
-                                                   valign => "top",
-                                                   wrapWidth => int $msgbox->getwidth - ($msgbox->getwidth * ($page->config->msgbox->{xmergin} / 50)),
-                                                   wrapHeight => int $msgbox->getheight - ($msgbox->getheight * ($page->config->msgbox->{ymergin} / 50)),
-                                                   xscale => 1,
-                                                   yscale => 1
-                                               );
-
-    #テキストの合成
-    $tb->draw(target => $msgbox,
-              x => $msgbox->getwidth * ($page->config->msgbox->{xmergin} / 100),
-              y => $msgbox->getheight * ($page->config->msgbox->{ymergin} /100),
-          );
-
-    #メッセージボックスの合成
-    $bg->rubthrough(src => $msgbox,
-                   tx => $page->config->size->{x} * ($page->config->msgbox->{x} / 100) - $msgbox->getwidth/2,
-                   ty => $page->config->size->{"y"} * ($page->config->msgbox->{"y"} / 100) - $msgbox->getheight/2,
-               ) or die $bg->errstr;
-
     return $bg;
 }
 
@@ -144,4 +146,5 @@ sub _write {
     $self->done($self->done + 1);
 
 }
+
 return 1;
